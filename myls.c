72621aca05665e1ed2baca8c2253f52b223f3756
kzabc <qq@kzabc.com>
2019-01-28 16:55:46 +0800

    学习getopt 解析参数

--- myls.c
+++ myls.c
@@ -6,7 +6,6 @@
 DIR *dp;
 struct dirent *dirp;
 void getDir(char *dir){
-    err_quit("usage : myls dir_name");
     if((dp = opendir(dir)) == NULL){
         err_quit("usage : myls dir_name");
     }else{
@@ -15,9 +14,41 @@ void getDir(char *dir){
         }
 
     }
+    closedir(dp);
 }
 int main(int argc, char *argv[]){
     printf("参数数量：%d，参数指针开始位置%p\n",argc,&argv);
+//    int ch;
+//    opterr=0;
+//    while((ch=getopt(argc,argv,":a::b::c::"))!=-1) {
+//        printf("optind: %d,argc:%d,argv[%d]:%s\n", optind,argc,optind,argv[optind]);
+//        switch(ch) {
+//            case 'a':
+//                printf("option a:%s\n", optarg);
+//                break;
+//            case 'b':
+//                printf("option b:%s\n", optarg);
+//                break;
+//            case 'c':
+//                printf("option c\n");
+//                break;
+//            case 'd':
+//                printf("option d\n");
+//                break;
+//            case 'e':
+//                printf("option e\n");
+//                break;
+//            case ':': //缺少选项
+//                printf("缺少选项参数\r\n");
+//                break;
+//
+//            default:
+//                printf("Knowed option:%s\n", optarg);
+//                break;
+//        }
+//        printf("optopt+%c\n",ch);
+//
+//    }
     char *buf = NULL;
     switch (argc){
         case 1 :
